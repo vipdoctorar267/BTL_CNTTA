137353c4552152e8e54c60c16cd64707434cae50
D521 <minhduchauik15@gmail.com>
2023-06-18 16:52:06 +0700

    upload project

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    public float speed = 5f; // Tốc độ di chuyển của nhân vật
+    public float stopTimeInSeconds = 2f; // Thời gian dừng lại (số giây)
+
+    private bool shouldMove = true;
+
+    void Start()
+    {
+        Invoke("StopMovement", stopTimeInSeconds);
+    }
+
+    void Update()
+    {
+        if (shouldMove)
+        {
+            // Di chuyển nhân vật về phía trước
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    void StopMovement()
+    {
+        shouldMove = false; // Dừng di chuyển
+        Debug.Log("Nhân vật đã dừng lại.");
+    }
+}
